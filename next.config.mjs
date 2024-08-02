c40bca3125ff3b755d6ce965821eac34fef0370b
TaehyunKim3 <agart319@gmail.com>
2024-08-02 16:43:42 +0900

    feat: svgr 설치 / 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['source.unsplash.com', 'images.unsplash.com', 'unsplash.com',], // 샘플 이미지용 도메인
+        minimumCacheTTL: 60 * 30, // 30분
+    },
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+        return config;
+    }
+};
 
 export default nextConfig;
